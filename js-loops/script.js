863fb5b7bfae86b7635177e2af0d07077e8e029b
Nicky Mortoza-Cowles <cinimodazotrom@gmail.com>
2024-09-04 14:58:15 +0100

    git add .

---
+++ js-loops/script.js
@@ -0,0 +1,7 @@
+//types of loops
+//!We always expect the looop to stop!!
+// for loop ---> a loop we use to repeat an action for a limited ammount of times
+// 
+//for (initialisation; condition; ){
+//   
+//}

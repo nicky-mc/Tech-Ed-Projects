d16efbd17d81b29748daf101c197038a06b8b6dc
Nicky Mortoza-Cowles <cinimodazotrom@gmail.com>
2024-09-10 13:02:36 +0000

    debugger update

---
+++ debugger/script.js
@@ -0,0 +1,11 @@
+//we want to test a function
+
+function countToTen() {
+  for (let i = 1; 1 <= 10; i++) {
+    console.log(i);
+    debugger;
+  }
+}
+countToTen();
+
+//this will stop tyhe code from running and we will use the dev tools to check
